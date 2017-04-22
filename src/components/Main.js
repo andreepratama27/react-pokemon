02690288619d30ae500fb55593c4bdc4e047593e
andreepratama27 <andreepratama27@gmail.com>
2017-04-22 11:47:53 +0700

    Edit modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,13 +20,22 @@ class Main extends React.Component {
     })
   }
 
+  handleClose () {
+    this.setState({
+      isShowingModal: false
+    })
+  }
+
   render () {
     return (
         <div className='pokedex-container'>
             {
               this.props.store.data.map((v, i) => {
                 return (
-                  <Pokedex data={v} id={i + 1} key={i} />
+                  <Pokedex
+                    data={v}
+                    id={i + 1}
+                    key={i} />
                 )
               })
             }
@@ -36,8 +45,8 @@ class Main extends React.Component {
             <div>
                 {
                   this.state.isShowingModal &&
-                  <ModalContainer onClose={this.handleClose}>
-                      <ModalDialog onClose={this.handleClose}>
+                  <ModalContainer onClose={ this.handleClose.bind(this) }>
+                      <ModalDialog onClose={ this.handleClose.bind(this) }>
                           <h1>Dialog Content</h1>
                           <p>More content.</p>
                       </ModalDialog>
