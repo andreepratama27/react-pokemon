f25a8159eda913d7692d72a8f1fe227a742389c4
andreepratama27 <andreepratama27@gmail.com>
2017-05-04 15:31:28 +0700

    Edit component, still paginate pokemon

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import { observer } from 'mobx-react'
 import Pokedex from 'components/Pokedex'
-import PokePlaceholder from 'components/PokePlaceholder'
 import {
   ModalContainer,
   ModalDialog
@@ -8,69 +7,44 @@ import {
 import InfiniteScroll from 'react-infinite-scroller'
 import Spinner from 'react-spinkit'
 import axios from 'axios'
+import store from './../PokeStore'
 
 @observer
 class Main extends React.Component {
   constructor (props) {
     super(props)
+    this.total = 0
+    this.loadPokemon = this.loadPokemon.bind(this)
+
     this.state = {
-      isshowingmodal: false,
-      load: false
+      data: []
     }
   }
 
-  componentWillMount () {
-    this.setState({
-      load: false
-    })
-  }
-
-  componentDidMount () {
-    this.setState({
-	   load: true
-    })
-  }
-
-  handleclick () {
-    this.setState({
-      isshowingmodal: true
-    })
-  }
-
-  handleclose () {
-    this.setState({
-      isshowingmodal: false
-    })
-  }
-
   loadPokemon () {
-    axios.get('http://pokeapi.salestock.net:8000/api/v2/pokemon/?offset=20')
-    .then(res => {
-      console.log(res.data.results)
-    })
+    this.total = this.total + 20
+  }
+
+  renderComponent () {
+    const { data } = this.props.store
+    return data.map((v, i) => <Pokedex data={v} id={i + 1} key={i} />)
   }
 
   render () {
-
-    const { data } = this.props.store
 
     return (
         <div className='pokedex-container'>
 
           <InfiniteScroll
             className='pokedex-scroller'
-            pageStart={0}
             loadMore={this.loadPokemon}
             hasMore={true}>
             {
-              this.state.load
-              ? data.map((v, i) => <Pokedex data={v} id={i + 1} key={i} />)
-              : <PokePlaceholder />
+              this.renderComponent()
             }
           </InfiniteScroll>
 
           <div className='pokedex-spinner'>
-
             <Spinner spinnerName='three-bounce' /> 
           </div>
 
